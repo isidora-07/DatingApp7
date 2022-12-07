a4fbbfc615017dd7a078b90c7e8fb6f49677bcee
Isidora Arandjelovic <arandjelovic_ika@hotmail.com>
2022-12-07 08:51:08 +0100

    End of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,4 @@
 using API.Data;
-using API.Entities;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,10 +11,14 @@ builder.Services.AddDbContext<DataContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
+
 app.MapControllers();
 
 app.Run();
+
